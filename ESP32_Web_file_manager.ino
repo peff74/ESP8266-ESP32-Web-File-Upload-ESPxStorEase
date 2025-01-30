6b896c208ea9c7217be40c1456a33ac8b0d3a735
peff74 <91614910+peff74@users.noreply.github.com>
2025-01-30 08:42:21 +0100

    Add files via upload

--- ESP32_Web_file_manager.ino
+++ ESP32_Web_file_manager.ino
@@ -2,17 +2,18 @@
 #include <WiFi.h>
 #include <LittleFS.h>
 #include <WebServer.h>
+#include <time.h>
 
 
 //=======Defines
 
 #define WIFI_SSID "Test"
 #define WIFI_PASSWORD "Password1"
-#define WiFiHOSTNAME "Test-ESP32"
 
 
 //=======SETTINGS
 
+WebServer server(80);
 
 /*
     Webserver
@@ -35,6 +36,42 @@ void serverSetup() {
   Serial.println("Server is now listening ...");
   Serial.println();
 }
+
+void handleRoot() {
+  if (LittleFS.exists("/index.html")) {
+    server.sendHeader("Location", "/main");
+    server.send(302, "text/plain", "Umleitung auf /main");
+  } else {
+    String redirectPage = "<html><body style='text-align:center;'>";
+    redirectPage += "<p>index.html wurde nicht gefunden.</p>";
+    redirectPage += "<p><a href='/fs'>File-Server</a></p>";
+    redirectPage += "</body></html>";
+
+    server.send(200, "text/html", redirectPage);
+  }
+}
+
+void handleReset() {
+  server.send(200, "text/plain", "RESET ESP in 2 Seconds...");
+  delay(2000);
+  ESP.restart();
+}
+
+void handleNotFound() {
+  String msg = "File Not Found\n\n";
+  msg += "URI: ";
+  msg += server.uri();
+  msg += "\nMethod: ";
+  msg += (server.method() == HTTP_GET) ? "GET" : "POST";
+  msg += "\nArguments: ";
+  msg += server.args();
+  msg += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    msg += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/html", msg);
+}
+
 /*
     File management
 */
@@ -52,12 +89,12 @@ void handleFileList() {
   File file = root.openNextFile();
 
   while (file) {
-    char buffer1[20]; 
+    char buffer1[20];
     size_t sizeofFile = file.size();
     formatBytes(sizeofFile, buffer1, sizeof(buffer1));
     //formatBytes(file.size(), sizeBuffer, sizeof(sizeBuffer));
     files += "<div class='file-container'>";
-    files += "<span class='filename'>" + String(file.name()) + "</span>";    
+    files += "<span class='filename'>" + String(file.name()) + "</span>";
     files += "<span class='file-size'>" + String(buffer1) + "</span>";
     files += "<span class='file-date'>" + getLastModified(file) + "</span>";
     files += "<span class='file-actions'>";
@@ -107,35 +144,28 @@ void handleFileUpload() {
   if (upload.status == UPLOAD_FILE_START) {
     String filename = "/" + upload.filename;
     Serial.println("Upload started: " + filename);
-    TelnetPrint.println("Upload started: " + filename);
 
     file = LittleFS.open(filename, "w");
     if (!file) {
       Serial.println(F("Failed to open file for writing"));
-      TelnetPrint.println(F("Failed to open file for writing"));
       return;
     }
     Serial.println(F("File opened for writing"));
-    TelnetPrint.println(F("File opened for writing"));
   } else if (upload.status == UPLOAD_FILE_WRITE) {
     if (file) {
       size_t bytesWritten = file.write(upload.buf, upload.currentSize);
       if (bytesWritten != upload.currentSize) {
         Serial.println(F("Error writing to file"));
-        TelnetPrint.println(F("Error writing to file"));
       } else {
         Serial.println("Bytes written to file: " + String(upload.currentSize));
-        TelnetPrint.println("Bytes written to file: " + String(upload.currentSize));
       }
     }
   } else if (upload.status == UPLOAD_FILE_END) {
     if (file) {
       file.close();
       Serial.println(F("Upload finished. File closed"));
-      TelnetPrint.println(F("Upload finished. File closed"));
     } else {
       Serial.println(F("Upload finished, but file was not open"));
-      TelnetPrint.println(F("Upload finished, but file was not open"));
     }
   }
 }
@@ -143,14 +173,11 @@ void handleFileUpload() {
 void handleFileDelete() {
   String filename = server.arg("file");
   Serial.println("Delete: " + filename);
-  TelnetPrint.println("Delete: " + filename);
 
   if (LittleFS.remove(filename)) {
     Serial.println(F("File deleted"));
-    TelnetPrint.println(F("File deleted"));
   } else {
     Serial.println(F("Failed to delete file"));
-    TelnetPrint.println(F("Failed to delete file"));
   }
   sendResponce();
 }
@@ -158,7 +185,6 @@ void handleFileDelete() {
 void handleFileView() {
   String filename = server.arg("file");
   Serial.println("View: " + filename);
-  TelnetPrint.println("View: " + filename);
 
   if (!filename.startsWith("/")) {
     filename = "/" + filename;
@@ -167,7 +193,6 @@ void handleFileView() {
   File file = LittleFS.open(filename, "r");
   if (!file) {
     Serial.println(F("Failed to open file for reading"));
-    TelnetPrint.println(F("Failed to open file for reading"));
     server.send(404, "text/plain", "File not found");
     return;
   }
@@ -182,7 +207,6 @@ void handleFileView() {
 void handleFormat() {
   LittleFS.format();
   Serial.println(F("Formating LittleFS"));
-  TelnetPrint.println(F("Formating LittleFS"));
   sendResponce();
 }
 
@@ -206,9 +230,19 @@ String getLastModified(File file) {
     End of Webserver
 */
 
-/*
-  WiFi
-*/
+// Readable display of memory sizes
+void formatBytes(size_t bytes, char *buffer, size_t bufferSize) {
+  if (bytes < 1024) {
+    snprintf_P(buffer, bufferSize, PSTR("%zu%s"), bytes, PSTR(" Byte"));
+  } else if (bytes < 1048576) {
+    dtostrf(static_cast<float>(bytes) / 1024.0, 6, 2, buffer);
+    strcat_P(buffer, PSTR(" KB"));
+  } else {
+    dtostrf(static_cast<float>(bytes) / 1048576.0, 6, 2, buffer);
+    strcat_P(buffer, PSTR(" MB"));
+  }
+}
+// Readable display of memory sizes
 
 
 
@@ -216,52 +250,19 @@ String getLastModified(File file) {
 
 void setup() {
   Serial.begin(115200);
-  WiFisetup();
-  WiFistartup();
-  ideOTASetup();
-  configTime(0, 0, "pool.ntp.org");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected! IP-Adresse: " + WiFi.localIP().toString());
   serverSetup();
   fileserverSetup();
+  configTime(0, 0, "pool.ntp.org");
 }
 
 //============LOOP
 
 void loop() {
-
-  if (WiFiconnected) {
-    ArduinoOTA.handle();
-  }
-
-  //===========Heartbeat
-  unsigned long currentMillis = millis();
-
-  if (currentMillis - HeartbeatMillis >= Heartbeatinterval) {
-    HeartbeatMillis = currentMillis;
-    --Measurement_count;
-    handelWiFi();
-
-    time_t now;
-    time(&now);
-    char ipBuffer[16];
-    WiFi.localIP().toString().toCharArray(ipBuffer, sizeof(ipBuffer));
-    snprintf(printbuffer0, sizeof(printbuffer0),
-             "WiFiconnected: %i \n"
-             "WiFi-OFF: %i \n"
-             "WiFi-OFF_count: %i \n"
-             "WiFi-Connecting_count: %i \n"
-             "Local IP:  %s \n"
-             "WiFi-RSSI:  %d \n"
-             "Measurement_count: %i \n"
-             "Unix-Time: %lld \n"
-             "___________________________",
-             WiFiconnected, WiFiOff, WiFiOff_count,
-             WiFiconnecting_count, ipBuffer, WiFi.RSSI(),
-             Measurement_count, now);
-    Serial.println(printbuffer0);
-
-
-    if (Measurement_count == 0) {
-      Measurement_count = MEASUREMENT_COUNT_VALUE;
-    }
-  }
+  server.handleClient();
 }
